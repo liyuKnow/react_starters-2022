7d1d231a33857da4d0e0e393a1a3b01a09a23410
liyuknow <kidus2011@gmail.com>
2022-09-12 17:28:07 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-
+import styled from 'styled-components';
 
 function App() {
+
+  // Create a Title component that'll render an <h1> tag with some styles
+  const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+  `;
+
+  // Create a Wrapper component that'll render a <section> tag with some styles
+  const Wrapper = styled.section`
+    padding: 4em;
+    background: papayawhip;
+  `;
+
   return (
-    <h1>Hello React Starter</h1>
+    <Wrapper>
+      <Title>
+        Hello Styled Components!
+      </Title>
+    </Wrapper>
   );
 }
 
